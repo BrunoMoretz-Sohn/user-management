7fde98f3e1fd662efdaa4abdd05e017ed8945ccb
BrunoMoretz-Sohn <moretzbruno@gmail.com>
2024-09-14 19:20:12 -0300

    feat(api): add axios instance and user service for API requests

---
+++ WEB/src/services/userService.js
@@ -0,0 +1,48 @@
+import api from '../services/api';
+import dayjs from 'dayjs';
+
+export async function getUsers() {
+  try {
+    const response = await api.get('/users');
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar todos os usuários:', error);
+    return [];
+  }
+}
+
+export async function createUser(name, email, birthDate) {
+  try {
+    const birthDateISO = dayjs(birthDate).toISOString();
+    await api.post('/users', { name, email, birthDate: birthDateISO });
+  } catch (error) {
+    console.error('Erro ao cadastrar usuário:', error);
+  }
+}
+
+export async function searchUser(param) {
+  try {
+    const response = await api.get(`/users/${param}`);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar usuário:', error);
+    return null;
+  }
+}
+
+export async function updateUser(id, name, email, birthDate) {
+  try {
+    const birthDateISO = dayjs(birthDate).toISOString();
+    await api.put(`/users/${id}`, { name, email, birthDate: birthDateISO });
+  } catch (error) {
+    console.error('Erro ao atualizar usuário:', error);
+  }
+}
+
+export async function deleteUser(id) {
+  try {
+    await api.delete(`/users/${id}`);
+  } catch (error) {
+    console.error('Erro ao deletar usuário:', error);
+  }
+}
