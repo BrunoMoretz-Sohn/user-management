2d337cb63d84e4573df5c387ac880e7f567eefb2
BrunoMoretz-Sohn <moretzbruno@gmail.com>
2024-09-14 16:40:43 -0300

    feat: define user CRUD routes on server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import { createUser } from './users/create.js';
+import { updateUser } from './users/update.js';
+import { deleteUser } from './users/delete.js';
+import { listAllUsers, getUserById, searchUser } from './users/list.js';
 
 const app = express();
 app.use(express.json());
+
+app.post('/users', createUser);
+app.put('/users/:id', updateUser);
+app.delete('/users/:id', deleteUser);
+app.get('/users', listAllUsers);
+app.get('/users/:id', getUserById);
+app.get('/users/search', searchUser);
 
 app.listen(3000);
