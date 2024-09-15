fda7351388105df908a1a0ca41dd4336e4c3c064
BrunoMoretz-Sohn <moretzbruno@gmail.com>
2024-09-15 18:30:21 -0300

    chore: update configuration for Render deployment

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { createUser } from './users/create.js';
 import { updateUser } from './users/update.js';
 import { deleteUser } from './users/delete.js';
 import { listAllUsers, getUserById, searchUser } from './users/list.js';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -15,6 +19,9 @@ app.delete('/users/:id', deleteUser);
 app.get('/users', listAllUsers);
 app.get('/users/:id', getUserById);
 app.get('/search', searchUser);
+
+const databaseUrl = process.env.DATABASE_URL;
+mongoose.connect(databaseUrl);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
